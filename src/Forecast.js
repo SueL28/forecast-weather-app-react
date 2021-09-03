2f2a51f8ee69f6f3375ad23bdd8e7727b1c3442d
SueL28 <69327405+SueL28@users.noreply.github.com>
2021-09-03 01:14:53 -0400

    Transferred from weather-app-reupload
    Starting to code in forecast for future days

---
+++ src/Forecast.js
@@ -0,0 +1,66 @@
+import axios from "axios";
+import React, {useState} from "react";
+import ConvertTemperature from "./ConvertTemperature";
+
+export default function Forecast(props){
+    const [forecast, setForecast] = useState("");
+    const[searched, setSearched]= useState(false);
+    let lat = props.lat;
+    let long = props.long;
+
+    let date = new Date();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    function getWeather(response){
+        setForecast(response.data.daily)
+        setSearched(true)
+        console.log(forecast)
+
+        
+    }
+
+
+    
+    
+    if (searched){
+        
+    return (
+        <div className="Forecast">
+          <div className="forecast-container">
+            <h3 className="forecast-font">7 Day Forecast</h3>
+    
+            <hr />
+    
+            <div className="row">
+              <div className="col-sm forecast-day-container">
+                
+                <span className="forecast-day">{days[(new Date(forecast[0].dt * 1000))]}</span>
+    
+                <div className="weather-status-font">{forecast[0].weather[0].description}</div>
+                <div>☀</div>
+                {/*<div><img className="emoji-forecast" src={props} alt={props}></img></div>*/}
+                <div className="forecast-high-number"><ConvertTemperature temp={Math.round(forecast[0].temp.max)}/></div>
+                <div className="weather-status-font">HIGH</div>
+                <div className="forecast-low-number"><ConvertTemperature temp={Math.round(forecast[0].temp.min)}/></div>
+                <p className="weather-status-font">LOW</p>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+
+        let apiKey = "a0ec055234934001bdc16c33f46f3ecb";
+        let forecastUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`;
+        axios.get(forecastUrl).then(getWeather)
+
+        return (
+            <div className="Forecast">
+                <h3 className="forecast-font">7 Day Forecast</h3>
+    
+                <hr />
+            </div>
+        );
+    }
+}
